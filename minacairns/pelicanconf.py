f27489501d7b47b4d40e768b81982e6735cf16f1
John Cairns <john@2ad.com>
2020-05-16 17:43:17 -0500

    adding mina

---
+++ minacairns/pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'John Cairns'
+SITENAME = u'Mighty Mina'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = u'en'
+
+WITH_FUTURE_DATES = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('John Cairns', 'https://2ad.com'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
