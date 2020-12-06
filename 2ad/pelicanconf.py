c5931116cfd7004949bd816b67373a48081fbcd9
John Cairns <john@2ad.com>
2020-12-05 18:16:17 -0600

    fixing sitemap settings

--- 2ad/pelicanconf.py
+++ 2ad/pelicanconf.py
@@ -12,7 +12,21 @@ SITELOGO = 'sitelogo.jpg'
 
 PLUGIN_PATHS = ['../pelican-plugins' ]
 
-PLUGINS = ['sitemap', 'post_stats', 'feed_summary']
+PLUGINS = ['sitemap', 'feed_summary']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 STATIC_PATHS = ('static',
                 'images',
