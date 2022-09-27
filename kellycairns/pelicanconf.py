af0aa365100b8303e4070666a87a955e01ddc888
John Cairns <john@2ad.com>
2022-09-27 12:37:33 -0500

    default pagination

--- kellycairns/pelicanconf.py
+++ kellycairns/pelicanconf.py
@@ -116,7 +116,7 @@ LINKS = (
 # not used in current theme
 # SOCIAL = ()
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = True
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
