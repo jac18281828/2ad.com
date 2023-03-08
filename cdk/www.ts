b3b5b44539a2031db931357b8019dfb64137861c
John Cairns <john@2ad.com>
2023-03-07 18:12:43 -0600

    fix deployment issues

--- cdk/www.ts
+++ cdk/www.ts
@@ -3,7 +3,7 @@ import * as cdk from '@aws-cdk/core';
 import { WwwStack } from './www-stack';
 
 const app = new cdk.App();
-new WwwStack(app, '2adStack', {
+new WwwStack(app, 'Stack2ad', {
   env: { account: '504242000181', region: 'us-east-2' },
 });
 
