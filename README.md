52aff423a9817578bb249e0126520c0cef7862a6
John Cairns <john@2ad.com>
2023-03-07 18:16:02 -0600

    add application load balancer

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # 2ad.com
 
 public code for (2ad website)[https://www.2ad.com]
+
+
+### Deployment
+
+1. `yarn install --dev`
+2. `cdk synth`
+3. `cdk deploy`
+...
+4. `cdk destroy`
--- cdk/www-stack.ts
+++ cdk/www-stack.ts
@@ -1,6 +1,8 @@
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import { CfnOutput } from '@aws-cdk/core';
 
 export class WwwStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -19,15 +21,39 @@ export class WwwStack extends cdk.Stack {
       memoryMiB: '1024',
     });
 
-    wwwTaskDefintion.addContainer('Container2ad', {
+    const container = wwwTaskDefintion.addContainer('Container2ad', {
       image: ecs.ContainerImage.fromRegistry('ghcr.io/jac18281828/2ad.com:latest'),
     });
 
-    new ecs.FargateService(this, 'Service2ad', {
+    container.addPortMappings({
+      containerPort: 80,
+      protocol: ecs.Protocol.TCP,
+    });
+
+    const service = new ecs.FargateService(this, 'Service2ad', {
       cluster: cluster,
       taskDefinition: wwwTaskDefintion,
       assignPublicIp: true,
       desiredCount: 1,
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'Alb2ad', {
+      vpc: vpc,
+      internetFacing: true,
+    });
+
+    const listener = lb.addListener('Listener', { port: 80 });
+    service.registerLoadBalancerTargets({
+      containerName: 'Container2ad',
+      containerPort: 80,
+      newTargetGroupId: 'ECS',
+      listener: ecs.ListenerConfig.applicationListener(listener, {
+        protocol: elbv2.ApplicationProtocol.HTTPS,
+      }),
+    });
+
+    new CfnOutput(this, 'LoadBalancerDNS', {
+      value: lb.loadBalancerDnsName,
     });
   }
 }
